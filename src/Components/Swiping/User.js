16593200a6edf7df8c645dae20f73aa3353e439f
Abigail Einhorn <abbyeinhorn@gmail.com>
2019-01-31 00:42:15 +0200

    more css

--- src/Components/Swiping/User.js
+++ src/Components/Swiping/User.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import { observer, inject } from 'mobx-react'
+import Carousel from 'nuka-carousel';
+
 import Axios from 'axios';
 
+@inject('UserData')
+@observer
 class User extends Component {
-    likingUser = (event) =>{
-        this.props.likingUser(event.target.value)
-        
+
+    likingUser = () => {
+        this.props.likingUser(this.props.user._id)
+
     }
     render() {
         const user = this.props.user
+
         return (
             <div>
-                <div className="user-info">
-                  <img src={user.imgURL}></img>
-                    <div>{user.name}</div>
-                    <div>{user.age}</div>
-                    <div>{user.location}</div>
-                    <div>{user.skills.map(skill => {
-                        return (
-                            <li>{skill}</li>
-                        )
-                    })}
-            
-                    </div>
-                <button onClick={this.likingUser}>dislike</button>
-                <button onClick={this.likingUser} value={user._id}>like</button>
+                {/* {this.props.UserData.currentUser !== this.props.user._id ? */}
+                <div className="swipe-card">
+                        <div >
+                    <Carousel>
+                        <div className="swipeUser-info" >
+                            <img src={user.imgURL}></img>
+                            <div>{user.name}</div>
+                            <div>{user.skills.map(skill => {
+                                return (
+                                    <li>{skill}</li>
+                                )
+                            })}
+                            </div>
+                            <div>{user.location}</div>
+                            <div>{user.age}</div>
+                        </div>
+                        <div className="reviews">
+                            insert reviews here for this specific user
+                            
+                         </div>
+                    </Carousel>
+                        </div>
 
+                    <button onClick={this.props.dislikeUser}>dislike</button>
+                    <button onClick={this.likingUser}>like</button>
                 </div>
+                {/* : null} */}
 
             </div>
         )
--- src/Components/UserPage/CurrentUser.js
+++ src/Components/UserPage/CurrentUser.js
@@ -5,10 +5,26 @@ import Axios from 'axios';
 class CurrentUser extends Component {
 
     render() {
+ 
         return (
             <div>
-                Current User
-           </div>
+                {/* <div className="swipeUser-info" >
+                            <img src={user.imgURL}></img>
+                            <div>{user.name}</div>
+                            <div>{user.skills.map(skill => {
+                                return (
+                                    <li>{skill}</li>
+                                )
+                            })}
+                            </div>
+                            <div>{user.location}</div>
+                            <div>{user.age}</div>
+                        </div>
+                        <div className="reviews">
+                            insert reviews here for this specific user
+                            
+                         </div>          */}
+                           </div>
 
         )
     }
