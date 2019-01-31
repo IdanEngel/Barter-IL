36c6f599a5cec2b2fe820c1db97b9be076d0ec35
Abigail Einhorn <abbyeinhorn@gmail.com>
2019-01-31 12:57:45 +0200

    added logout

--- src/Components/Chats/Chats.js
+++ src/Components/Chats/Chats.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Chatlist from './ChatList';
+import NavBar from '../NavBar';
 
 class Chats extends Component {
   // constructor(){
@@ -12,9 +13,12 @@ class Chats extends Component {
   // }
   render() {
     return (
+      <div>
+        <NavBar />
       <div className="chats">
         <h1>Chats</h1>
         <img className="chatImg" src="https://static1.squarespace.com/static/58297336c534a550aa00672c/t/5b0ff738f950b73249f9bf38/1527773020353/girl+with+empty+tinder+profile"></img>
+      </div>
       </div>
     );
   }
--- src/Components/UserPage/CurrentUser.js
+++ src/Components/UserPage/CurrentUser.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import Axios from 'axios';
 import NavBar from '../NavBar';
 
 class CurrentUser extends Component {
+    constructor() {
+        super()
+        this.state = {
+            val: false
+        }
+    }
 
+    clearLS = () => {
+        localStorage.clear()
+        this.setState({
+            val: !this.state.val
+        })
+    }
     render() {
- 
+        let storage = localStorage.getItem(`username`)
         return (
             <div>
-                <NavBar />
+            <NavBar />
+            <h1>Current User</h1>
             <div className="currentUserPage">
-                <h1>Shoobert</h1>
+                <h2>Shoobert</h2>
                 <img src="https://media.licdn.com/dms/image/C5603AQFeD2Nyr-56vw/profile-displayphoto-shrink_200_200/0?e=1551312000&v=beta&t=3HpazftYEi9LINAIQa4kyWxj2kzz_qCpkExKtIjjpco"></img>
              </div>
+             {storage ?
+                        null :
+                        <Redirect to='/' />}
+                <button onClick={this.clearLS}>Logout</button>
              </div>
         )
     }
