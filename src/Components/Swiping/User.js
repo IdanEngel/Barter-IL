5956d2cf6a79eba80558484f0a7bd640cb0acb88
Idan Engel <you@example.com>
2019-01-31 11:45:42 +0200

    login completed

--- src/Components/Swiping/User.js
+++ src/Components/Swiping/User.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import Axios from 'axios';
 
 class User extends Component {
+    constructor() {
+        super()
+        this.state = {
+            val: false
+        }
+    }
     likingUser = (event) =>{
         this.props.likingUser(event.target.value)
         
     }
+    clearLS = () =>{
+        localStorage.clear()
+        this.setState({
+            val: !this.state.val
+        })
+
+    }
     render() {
         const user = this.props.user
+        let storage = localStorage.getItem(`username`)
         return (
             <div>
+                 {storage ?
+                        null :
+                        <Redirect to='/' />}
+                <button onClick={this.clearLS}>Logout</button>
                 <div className="user-info">
                   <img src={user.imgURL}></img>
                     <div>{user.name}</div>
@@ -24,7 +42,8 @@ class User extends Component {
             
                     </div>
                 <button onClick={this.likingUser}>dislike</button>
-                <button onClick={this.likingUser} value={user._id}>like</button>
+                <button onClick={this.likingUser} value={user._id}>like</button> <br/>
+
 
                 </div>
 
