0c3c03e1c440e779bf6ae4eb05dc741bb39e8b30
Abigail Einhorn <abbyeinhorn@gmail.com>
2019-02-03 16:46:19 +0200

    more route changes

--- src/Components/Chats/Matches.js
+++ src/Components/Chats/Matches.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Chats.css'
+import { observer, inject } from 'mobx-react'
 
 
-
+@inject('UserData')
+@observer
 class Matches extends Component {
     render() {
         return (
             <div className="singleMatch-imgDiv">
+                <Link to={"/chatslists/chatWindow/" + this.props.user.username}>
                     <img className="match-img" src={this.props.user.imgURL}></img>
+                </Link>
             </div>
 
         )
