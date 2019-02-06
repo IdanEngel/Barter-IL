7f1e16fbbb06ce962ae88a16fd3ab9a92ca9053b
yanivsel <yanivsel45@gmail.com>
2019-02-06 20:12:30 +0200

    current user page css is almost complete

--- src/Components/UserPage/CurrentUser.js
+++ src/Components/UserPage/CurrentUser.js
@@ -6,6 +6,7 @@ import NavBar from '../NavBar';
 import { observer, inject } from 'mobx-react';
 import { async } from 'q';
 
+
 @inject('userLogin', 'UserData', 'currentUserData')
 @observer
 class CurrentUser extends Component {
@@ -45,7 +46,7 @@ class CurrentUser extends Component {
         let finalUserData = this.props.currentUserData.currentUserInformation
         let finalSkillData = this.props.currentUserData.skillsData
         // console.log(finalSkillData);
-        let skillData = finalSkillData.map(m =><div>{m} </div>)
+        let skillData = finalSkillData.map(m =><div className="skill">{m} </div>)
         
         return (
             <div>
@@ -54,19 +55,20 @@ class CurrentUser extends Component {
                         <Redirect to='/' />}
                 <NavBar />
                 <div className="currentUserPage">
-                    <img className="currentUser-img" src={finalUserData.imgURL} alt="currentUser-img"></img>
-                    <h3>Name: {finalUserData.name} {finalUserData.lastName}
-                    <br></br>
-                    Location: {finalUserData.location}
-                    <br></br>
-                    Age: {finalUserData.age}
-                    </h3>
-                    <h4>Skills: {skillData}</h4>
+                <div className="header">
+                <img className="currentUser-img" src={finalUserData.imgURL} alt="currentUser-img"></img>
+                <div className="name">{finalUserData.name}</div>
+                </div>
+                <div className="main">
+                    <div className="location">{finalUserData.location}</div>
+                    <div className="age">{finalUserData.age}</div>
+                    <div className="skills"><strong>Skills:</strong> {skillData}</div>
+                </div>
                    
                     {storage ?
                         null :
                         <Redirect to='/' />}
-                    <button onClick={this.clearLS}>Logout</button>
+                    <button className="logout" onClick={this.clearLS}>Logout</button>
                 </div>
             </div>
         )
