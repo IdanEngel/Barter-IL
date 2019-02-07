c3e5c97c3997ae3e4caa8c4b797a92986d6d16eb
Abigail Einhorn <abbyeinhorn@gmail.com>
2019-02-07 12:31:46 +0200

    css fix

--- src/Components/Landing/Signup.js
+++ src/Components/Landing/Signup.js
@@ -24,24 +24,24 @@ class Signup extends Component {
         return (
             <div>
                 <NavBar />
-                <h2>Sign Up</h2>
+                {/* <h2>Sign Up</h2> */}
                 <div className="add-users">
                     <label for="username">Username: </label>
-                    <input id="username" type="text" onChange={this.updatState} placeholder="username" name='username' />
+                    <input id="username" type="text" onChange={this.updatState} placeholder="elevation123" name='username' />
                     <label for="password">Password: </label>
                     <input id="password" type="password" onChange={this.updatState} placeholder="password" name='password' />
                     <label for="FN">First Name: </label>
-                    <input id="FN" type="text" onChange={this.updatState} placeholder="First Name" name='name' />
+                    <input id="FN" type="text" onChange={this.updatState} placeholder="John" name='name' />
                     <label for="SN"> Last name: </label>
-                    <input id="SN" type="text" onChange={this.updatState} placeholder="lastname" name='lastname' />
+                    <input id="SN" type="text" onChange={this.updatState} placeholder="Smith" name='lastname' />
                     <label for="location">Location: </label>
-                    <input id="location" type="text" onChange={this.updatState} placeholder="location" name='location' />
+                    <input id="location" type="text" onChange={this.updatState} placeholder="Las Vegas" name='location' />
                     <label for="skills">Skills: </label>
-                    <input id="skills" type="text" onChange={this.updatState} placeholder="skills" name='skills' />
+                    <input id="skills" type="text" onChange={this.updatState} placeholder="eating, snorkling..." name='skills' />
                     <label for="age">Age: </label>
-                    <input id="age" type="number" onChange={this.updatState} placeholder="age" name='age' />
+                    <input id="age" type="number" onChange={this.updatState} placeholder="120" name='age' />
                     <label for="image">Image: </label>
-                    <input id="image" type="text" onChange={this.updatState} placeholder="image" name='imgURL' />
+                    <input id="image" type="text" onChange={this.updatState} placeholder="enter url" name='imgURL' />
 
                 </div>
                     <button className="signup-button" onClick={this.addNewUser}>Signup</button>
